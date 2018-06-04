6c1f0a1757055fcead8ed70153d8ae2aa6b07bb1
Vishnu Adda <addavishnu@gmail.com>
2018-06-03 22:49:48 -0700

    General layout of both Activities has been determined.

--- app/src/main/java/com/someapp/vishnu/mynotesapp/MainActivity.java
+++ app/src/main/java/com/someapp/vishnu/mynotesapp/MainActivity.java
@@ -1,13 +1,81 @@
 package com.someapp.vishnu.mynotesapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+    static ArrayList<String> notes = new ArrayList<String>();
+    static ArrayAdapter arrayAdapter;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.addNote:
+                Intent intent = new Intent(getApplicationContext(), NotesActivity.class);
+
+                startActivity(intent);
+                Toast.makeText(getApplicationContext(),
+                        "Note added!", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedPreferences = this.getSharedPreferences(
+                "com.someapp.vishnu.mynotesapp", Context.MODE_PRIVATE);
+
+        notes = new ArrayList<String>();
+
+        ListView listView = findViewById(R.id.notesListView);
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent = new Intent(getApplicationContext(), NotesActivity.class);
+                intent.putExtra("note", position);
+
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
